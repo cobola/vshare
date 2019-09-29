588996bbb9c0575ee3d31db164c4957c6f0700e7
yangjf <cobola@126.com>
2019-09-29 10:12:29 +0800

    fix  baidu http js url

--- src/lib/vshare.vue
+++ src/lib/vshare.vue
@@ -6,40 +6,44 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       defaultConfig: {
-        shareList: ['more','qzone','tsina','tqq','renren','weixin'],
-        share: [{bdSize: 24}],
+        shareList: ["more", "qzone", "tsina", "tqq", "renren", "weixin"],
+        share: [{ bdSize: 24 }],
         slide: false,
         image: false,
         selectShare: false
       }
-    }
+    };
   },
-  props: ['vshareConfig'],
-  mounted () {
-    console.log(this.vshareConfig)
+  props: ["vshareConfig"],
+  mounted() {
+    console.log(this.vshareConfig);
     if (this.vshareConfig) {
-      this.vshareConfig.shareList ? this.defaultConfig.shareList = this.vshareConfig.shareList :'';
+      this.vshareConfig.shareList
+        ? (this.defaultConfig.shareList = this.vshareConfig.shareList)
+        : "";
       if (this.vshareConfig.shareList) {
-
       }
       if (this.vshareConfig.share) {
-        this.defaultConfig.share = this.vshareConfig.share
+        this.defaultConfig.share = this.vshareConfig.share;
       }
       if (this.vshareConfig.slide) {
-        this.defaultConfig.slide = this.vshareConfig.slide
+        this.defaultConfig.slide = this.vshareConfig.slide;
       }
       if (this.vshareConfig.image) {
-        this.defaultConfig.image = this.vshareConfig.image
+        this.defaultConfig.image = this.vshareConfig.image;
       }
       if (this.vshareConfig.selectShare) {
-        this.defaultConfig.selectShare = this.vshareConfig.selectShare
+        this.defaultConfig.selectShare = this.vshareConfig.selectShare;
       }
     }
-    window._bd_share_config = this.defaultConfig
-    document.getElementsByTagName('body')[0].appendChild(document.createElement('script')).src = 'http://bdimg.share.baidu.com/static/api/js/share.js?cdnversion='+~(-new Date()/36e5)
+    window._bd_share_config = this.defaultConfig;
+    document
+      .getElementsByTagName("body")[0]
+      .appendChild(document.createElement("script")).src =
+      "//node2d-public.hep.com.cn/share.js?cdnversion=" + ~(-new Date() / 36e5);
   }
-}
+};
 </script>
